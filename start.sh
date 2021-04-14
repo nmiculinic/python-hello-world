2d2a525cff37482647d3a6966c83acd1115eb25f
Neven Miculinic <neven.miculinic@gmail.com>
2021-04-14 17:58:05 +0200

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 for i in {1..10}
 do 
-grid train main.py --grid_instance_type t2.medium --learning_rate "uniform(1e-5, 1e-1, 1)"
+grid train main.py --grid_cpus 1 --grid_instance_type t2.medium --learning_rate "uniform(1e-5, 1e-1, 1)"
 
 done
